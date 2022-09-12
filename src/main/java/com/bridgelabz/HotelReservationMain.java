1097112d34e8062a98b29c850d813f1ac0475a89
Surabhi951 <surabhibhagat95@gmail.com>
2022-09-12 19:15:29 +0530

    Added hotel

--- src/main/java/com/bridgelabz/HotelReservationMain.java
+++ src/main/java/com/bridgelabz/HotelReservationMain.java
@@ -3,5 +3,8 @@ package com.bridgelabz;
 public class HotelReservationMain {
     public static void main(String[] args){
         System.out.println("Welcome to hotel reservation system");
+        HotelReservationService hotelReservationService = new HotelReservationService();
+        hotelReservationService.addHotel(new Hotel("Lakewood", 110));
+
     }
 }
