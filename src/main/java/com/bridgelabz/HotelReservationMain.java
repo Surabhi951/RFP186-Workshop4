10d1e697502523cd02cf8bfd066dafd1a2af079f
Surabhi951 <surabhibhagat95@gmail.com>
2022-09-13 17:10:53 +0530

    Added weekday and weekend rates for each hotel

--- src/main/java/com/bridgelabz/HotelReservationMain.java
+++ src/main/java/com/bridgelabz/HotelReservationMain.java
@@ -7,15 +7,14 @@ public class HotelReservationMain {
         System.out.println("Welcome to hotel reservation system");
         Scanner sc = new Scanner(System.in);
         HotelReservationService hotelReservationService = new HotelReservationService();
-        hotelReservationService.addHotel(new Hotel("Lakewood", 110));
-        hotelReservationService.addHotel(new Hotel("Bridgewood", 160));
-        hotelReservationService.addHotel(new Hotel("Ridgewood", 220));
+        hotelReservationService.addHotel(new Hotel("Lakewood", 110, 90));
+        hotelReservationService.addHotel(new Hotel("Bridgewood", 150, 60));
+        hotelReservationService.addHotel(new Hotel("Ridgewood", 220, 150));
 
-        System.out.println("Enter check-in date: (dd-mm=yyyy)");
+        System.out.println("Enter check-in date: (dd-mm-yyyy)");
         String checkInDate = sc.nextLine();
-        System.out.println("Enter check-out date: (dd-mm=yyyy)");
+        System.out.println("Enter check-out date: (dd-mm-yyyy)");
         String checkOutDate = sc.nextLine();
         Hotel cheapestHotel = hotelReservationService.getCheapestHotel(checkInDate, checkOutDate);
-        System.out.println("Cheapest Hotel: " + cheapestHotel.getName() + ", Total Cost: " + cheapestHotel.getTotalCost());
-    }
+        System.out.println("Cheapest Hotel: " + cheapestHotel.getName() + ", Total Cost: " + cheapestHotel.getTotalCost()); }
 }
