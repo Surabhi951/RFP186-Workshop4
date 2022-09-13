1528364472cadbdda712bdce5fd63be46b3d5da1
Surabhi951 <surabhibhagat95@gmail.com>
2022-09-13 16:02:06 +0530

    Added: to find the cheapest hotel for given date range

--- src/main/java/com/bridgelabz/Hotel.java
+++ src/main/java/com/bridgelabz/Hotel.java
@@ -1,11 +1,28 @@
 package com.bridgelabz;
 
 public class Hotel {
-    String name;
-    int rate;
+    private final String name;
+    private final int rate;
+    private int totalCost;
 
     public Hotel(String name, int rate) {
         this.name = name;
         this.rate = rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(int totalCost) {
+        this.totalCost = totalCost;
     }
 }
---
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -0,0 +1,30 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class HotelReservationTest {
+    static HotelReservationService hotelReservationService;
+
+    @BeforeAll
+    static void initialize() {
+        hotelReservationService = new HotelReservationService();
+        hotelReservationService.addHotel(new Hotel("Lakewood", 110));
+        hotelReservationService.addHotel(new Hotel("Bridgewood", 160));
+        hotelReservationService.addHotel(new Hotel("Ridgewood", 220));
+    }
+
+    @Test
+    void givenHotel_ShouldBe_AddedToHotelList() {
+        boolean result = hotelReservationService.addHotel(new Hotel("Lakewood", 110));
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void givenCheckInAndCheckOutDates_userShould_GetCheapestHotel() {
+        Hotel hotel = hotelReservationService.getCheapestHotel("23-05-2022", "31-05-2022");
+        Assertions.assertEquals("Lakewood", hotel.getName());
+    }
+
+}
